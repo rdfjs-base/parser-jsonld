ac725ae3e0f9e9ab3d32677aef64782b04373e57
bergi <bergi@axolotlfarm.org>
2015-09-05 22:33:33 +0200

    use standard code style

--- index.js
+++ index.js
@@ -1,217 +1,211 @@
-var
-  jsonld = require('jsonld'),
-  AbstractParser = require('rdf-parser-abstract');
-
+var jsonld = require('jsonld')
+var AbstractParser = require('rdf-parser-abstract')
 
 var JsonLdParser = function (rdf, options) {
-  AbstractParser.call(this);
+  AbstractParser.call(this, rdf)
 
-  options = options || {};
+  options = options || {}
 
   if (!('importPrefixMap' in options)) {
-    options.importPrefixMap = true;
+    options.importPrefixMap = true
   }
 
   var expandAndFlatten = function (data, base, callback) {
     jsonld.expand(data, {'base': base}, function (error, expanded) {
       if (error) {
-        return callback(error);
+        return callback(error)
       }
 
       jsonld.flatten(expanded, {}, function (error, flattened) {
         if (error) {
-          return callback(error);
+          return callback(error)
         }
 
         if (!('@graph' in flattened)) {
-          return callback(new Error('no @graph property in flattened JSON-LD'));
+          return callback(new Error('no @graph property in flattened JSON-LD'))
         }
 
-        callback(null, flattened['@graph']);
-      });
-    });
-  };
+        callback(null, flattened['@graph'])
+      })
+    })
+  }
 
   var toArray = function (object) {
-    object = object || [];
+    object = object || []
 
     if (Array.isArray(object)) {
-      return object;
+      return object
     }
 
-    return [object];
-  };
+    return [object]
+  }
 
   this.process = function (data, callback, base, filter, done) {
     return new Promise(function (resolve, reject) {
       if (typeof data === 'string') {
-        data = JSON.parse(data);
+        data = JSON.parse(data)
       }
 
-      base = base || '';
-      filter = filter || function () { return true; };
-      done = done || function () {};
+      base = base || ''
+      filter = filter || function () { return true }
+      done = done || function () {}
 
       var getLiteral = function (jsonNode) {
-        var
-          type = null,
-          lang = null;
+        var type = null
+        var lang = null
 
         if ('@type' in jsonNode) {
-          type = getNode(jsonNode['@type']);
+          type = getNode(jsonNode['@type'])
         }
 
         if ('@language' in jsonNode) {
-          lang = jsonNode['@language'];
+          lang = jsonNode['@language']
         }
 
-        return rdf.createLiteral(jsonNode['@value'], lang, type);
-      };
+        return rdf.createLiteral(jsonNode['@value'], lang, type)
+      }
 
-      var nodeCache = {};
+      var nodeCache = {}
 
       var getNode = function (jsonNode) {
         // is there already a node?
         if (jsonNode in nodeCache) {
-          return nodeCache[jsonNode];
+          return nodeCache[jsonNode]
         }
 
         // is it a blank node?
         if (!jsonNode) {
-          return rdf.createBlankNode();
+          return rdf.createBlankNode()
         }
 
         if (jsonNode.indexOf('_:') === 0) {
-          return (nodeCache[jsonNode] = rdf.createBlankNode());
+          return (nodeCache[jsonNode] = rdf.createBlankNode())
         }
 
         // if not it's a named node
-        return (nodeCache[jsonNode] = rdf.createNamedNode(jsonNode));
-      };
+        return (nodeCache[jsonNode] = rdf.createNamedNode(jsonNode))
+      }
 
       var pushTriple = function (subject, predicate, object) {
-        var triple = rdf.createTriple(subject, predicate, object);
+        var triple = rdf.createTriple(subject, predicate, object)
 
         if (filter(triple)) {
-          callback(triple);
+          callback(triple)
         }
-      };
+      }
 
       var processSubject = function (jsonSubject) {
-        var
-          subject = jsonSubject['@id'],
-          types = toArray(jsonSubject['@type']);
+        var subject = jsonSubject['@id']
+        var types = toArray(jsonSubject['@type'])
 
         // add type triples
         types.forEach(function (type) {
           pushTriple(
             getNode(subject),
             getNode(rdf.ns.type),
-            getNode(type));
-        });
+            getNode(type))
+        })
 
         // other triples
         for (var predicate in jsonSubject) {
           // ignore JSON-LD properties
           if (predicate.indexOf('@') === 0) {
-            continue;
+            continue
           }
 
-          processPredicate(subject, predicate, toArray(jsonSubject[predicate]));
+          processPredicate(subject, predicate, toArray(jsonSubject[predicate]))
         }
-      };
+      }
 
       var processPredicate = function (subject, predicate, jsonObjects) {
         jsonObjects.forEach(function (jsonObject) {
           pushTriple(
             getNode(subject),
             getNode(predicate),
-            processObject(jsonObject));
-        });
-      };
+            processObject(jsonObject))
+        })
+      }
 
       var processObject = function (jsonObject) {
         // is it a simple literal?
         if (typeof jsonObject === 'string') {
-          return rdf.createLiteral(jsonObject);
+          return rdf.createLiteral(jsonObject)
         }
 
         // or blank node / named node
         if ('@id' in jsonObject) {
-          return getNode(jsonObject['@id']);
+          return getNode(jsonObject['@id'])
         }
 
         if ('@list' in jsonObject) {
-          return processList(jsonObject['@list']);
+          return processList(jsonObject['@list'])
         }
 
         // or complex literal
-        return getLiteral(jsonObject);
-      };
+        return getLiteral(jsonObject)
+      }
 
       var processList = function (jsonList) {
-        var
-          entry = getNode(),
-          subject = entry,
-          rest;
+        var entry = getNode()
+        var subject = entry
+        var rest
 
         jsonList.forEach(function (jItem, index) {
           if (index !== jsonList.length - 1) {
-            rest = getNode();
+            rest = getNode()
           } else {
-            rest = getNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#nil');
+            rest = getNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#nil')
           }
 
           pushTriple(
             subject,
             getNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-            getNode(jItem['@id']));
+            getNode(jItem['@id']))
 
           pushTriple(
             subject,
             getNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#rest'),
-            rest);
+            rest)
 
-          subject = rest;
-        });
+          subject = rest
+        })
 
-        return entry;
-      };
+        return entry
+      }
 
       if (options.importPrefixMap && '@context' in data && typeof data['@context'] === 'object') {
-        var context = data['@context'];
+        var context = data['@context']
 
         Object.keys(context).forEach(function (key) {
           if (key.slice(0, 1) !== '@' && typeof context[key] === 'string') {
-            rdf.prefixes[key] = context[key];
+            rdf.prefixes[key] = context[key]
           }
-        });
+        })
       }
 
       expandAndFlatten(data, base, function (error, jsonGraph) {
         if (error) {
           if (done) {
-            done(error);
+            done(error)
           }
 
-          return reject(error);
+          return reject(error)
         }
 
         if (jsonGraph) {
           jsonGraph.forEach(function (jsonSubject) {
-            processSubject(jsonSubject);
-          });
+            processSubject(jsonSubject)
+          })
         }
 
         if (done) {
-          done();
+          done()
         }
 
-        resolve();
-      });
-    });
-  };
-};
+        resolve()
+      })
+    })
+  }
+}
 
-
-module.exports = JsonLdParser;
+module.exports = JsonLdParser
--- test/test.js
+++ test/test.js
@@ -1,264 +1,233 @@
-var
-  assert = require('assert'),
-  rdf = require('rdf-ext')(),
-  testData = require('rdf-test-data')(rdf),
-  testUtils = require('rdf-test-utils')(rdf);
-  JsonLdParser = require('../').bind(null, rdf);
+/* global describe, it */
 
+var assert = require('assert')
+var rdf = require('rdf-ext')()
+var testData = require('rdf-test-data')(rdf)
+var testUtils = require('rdf-test-utils')(rdf)
+var JsonLdParser = require('../').bind(null, rdf)
 
 describe('JSON-LD parser', function () {
   describe('process API', function () {
     it('should be supported', function (done) {
-      var parser = new JsonLdParser();
-      var counter = 0;
+      var parser = new JsonLdParser()
+      var counter = 0
 
       parser.process({
         '@id': 'http://example.org/subject',
         'http://example.org/predicate': 'object'
       }, function () {
-        counter++;
+        counter++
+      }).then(function () {
+        if (counter !== 1) {
+          done('no triple processed')
+        } else {
+          done()
+        }
+      }).catch(function (error) {
+        done(error)
       })
-        .then(function () {
-          if (counter !== 1) {
-            done('no triple processed');
-          } else {
-            done();
-          }
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
+    })
 
     it('should use base parameter', function (done) {
-      var parser = new JsonLdParser();
-      var counter = 0;
+      var parser = new JsonLdParser()
+      var counter = 0
 
       parser.process({
         '@id': 'subject',
         'http://example.org/predicate': 'object'
       }, function (triple) {
         if (triple.subject.toString() === 'http://example.org/subject') {
-          counter++;
+          counter++
         }
-      }, 'http://example.org/')
-        .then(function () {
-          if (counter !== 1) {
-            done('no triple processed');
-          } else {
-            done();
-          }
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
+      }, 'http://example.org/').then(function () {
+        if (counter !== 1) {
+          done('no triple processed')
+        } else {
+          done()
+        }
+      }).catch(function (error) {
+        done(error)
+      })
+    })
 
     it('should use filter parameter', function (done) {
-      var parser = new JsonLdParser();
-      var counter = 1;
+      var parser = new JsonLdParser()
+      var counter = 1
 
       parser.process({
         '@id': 'http://example.org/subject',
         'http://example.org/predicate': 'object'
       }, function () {
-        counter *= 2;
+        counter *= 2
       }, null, function () {
-        counter *= 3;
+        counter *= 3
 
-        return false;
+        return false
+      }).then(function () {
+        if (counter !== 3) {
+          done('no triple processed')
+        } else {
+          done()
+        }
+      }).catch(function (error) {
+        done(error)
       })
-        .then(function () {
-          if (counter !== 3) {
-            done('no triple processed');
-          } else {
-            done();
-          }
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
+    })
 
     it('should use done parameter', function (done) {
-      var parser = new JsonLdParser();
-      var counter = 0;
+      var parser = new JsonLdParser()
+      var counter = 0
 
-      (new Promise(function (resolve) {
+      Promise.resolve(new Promise(function (resolve) {
         parser.process({
           '@id': 'http://example.org/subject',
           'http://example.org/predicate': 'object'
         }, function () {
-          counter++;
+          counter++
         }, null, null, function () {
-          resolve();
+          resolve()
         })
-      }))
-        .then(function () {
-          if (counter !== 1) {
-            done('no triple processed');
-          } else {
-            done();
-          }
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
-  });
+      })).then(function () {
+        if (counter !== 1) {
+          done('no triple processed')
+        } else {
+          done()
+        }
+      }).catch(function (error) {
+        done(error)
+      })
+    })
+  })
 
   describe('callback API', function () {
     it('should be supported', function (done) {
-      var parser = new JsonLdParser();
+      var parser = new JsonLdParser()
 
-      (new Promise(function (resolve) {
+      Promise.resolve(new Promise(function (resolve) {
         parser.parse({}, function () {
-          resolve();
-        });
-      }))
-        .then(function () {
-          done()
+          resolve()
         })
-        .catch(function (error) {
-          done(error);
-        });
-    });
+      })).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
 
     it('should forward errors', function (done) {
-      var parser = new JsonLdParser();
+      var parser = new JsonLdParser()
 
-      (new Promise(function (resolve, reject) {
+      Promise.resolve(new Promise(function (resolve, reject) {
         parser.parse('{"@context": "urn:test"}', function (error) {
           if (error) {
-            reject(error);
+            reject(error)
           } else {
-            resolve();
+            resolve()
           }
         })
-      }))
-        .then(function () {
-          done('no error thrown');
-        })
-        .catch(function () {
-          done();
-        });
-    });
-  });
+      })).then(function () {
+        done('no error thrown')
+      }).catch(function () {
+        done()
+      })
+    })
+  })
 
   describe('Promise API', function () {
     it('should be supported', function (done) {
-      var parser = new JsonLdParser();
+      var parser = new JsonLdParser()
 
-      parser.parse({})
-        .then(function () {
-          done()
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
+      parser.parse({}).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
 
     it('should forward error to Promise API', function (done) {
-      var parser = new JsonLdParser();
+      var parser = new JsonLdParser()
 
-      parser.parse('{"@context": "urn:test"}')
-        .then(function () {
-          done('no error thrown');
-        })
-        .catch(function () {
-          done();
-        });
-    });
-  });
+      parser.parse('{"@context": "urn:test"}').then(function () {
+        done('no error thrown')
+      }).catch(function () {
+        done()
+      })
+    })
+  })
 
   describe('Stream API', function () {
     it('should be supported', function (done) {
-      var parser = new JsonLdParser();
-      var counter = 0;
+      var parser = new JsonLdParser()
+      var counter = 0
 
       parser.stream({
         '@id': 'http://example.org/subject',
         'http://example.org/predicate': 'object'
+      }).on('data', function () {
+        counter++
+      }).on('end', function () {
+        if (counter !== 1) {
+          done('no triple streamed')
+        } else {
+          done()
+        }
+      }).on('error', function (error) {
+        done(error)
       })
-        .on('data', function () {
-          counter++;
-        })
-        .on('end', function () {
-          if (counter !== 1) {
-            done('no triple streamed');
-          } else {
-            done();
-          }
-        })
-        .on('error', function (error) {
-          done(error);
-        });
-    });
-  });
+    })
+  })
 
   describe('example data', function () {
     it('card.json should be parsed', function (done) {
-      var parser = new JsonLdParser();
+      var parser = new JsonLdParser()
 
-      testUtils.p.readFile('support/card.json', __dirname)
-        .then(function (card) {
-          return parser.parse(card, null, 'https://www.example.com/john/card')
-        })
-        .then(function (graph) {
-          return testUtils.p.assertGraphEqual(graph, testData.cardGraph);
-        })
-        .then(function () {
-          done()
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
+      testUtils.p.readFile('support/card.json', __dirname).then(function (card) {
+        return parser.parse(card, null, 'https://www.example.com/john/card')
+      }).then(function (graph) {
+        return testUtils.p.assertGraphEqual(graph, testData.cardGraph)
+      }).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
 
     it('card.json should feed prefix map', function (done) {
-      var parser = new JsonLdParser();
+      var parser = new JsonLdParser()
 
       if (rdf.prefixes.cert) {
-        delete rdf.prefixes.cert;
+        delete rdf.prefixes.cert
       }
 
       if (rdf.prefixes.foaf) {
-        delete rdf.prefixes.foaf;
+        delete rdf.prefixes.foaf
       }
 
-      testUtils.p.readFile('support/card.json', __dirname)
-        .then(function (card) {
-          return parser.parse(card, null, 'https://www.example.com/john/card')
-        })
-        .then(function () {
-          assert.equal(rdf.prefixes.cert, 'http://www.w3.org/ns/auth/cert#');
-          assert.equal(rdf.prefixes.foaf, 'http://xmlns.com/foaf/0.1/');
-        })
-        .then(function () {
-          done()
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
+      testUtils.p.readFile('support/card.json', __dirname).then(function (card) {
+        return parser.parse(card, null, 'https://www.example.com/john/card')
+      }).then(function () {
+        assert.equal(rdf.prefixes.cert, 'http://www.w3.org/ns/auth/cert#')
+        assert.equal(rdf.prefixes.foaf, 'http://xmlns.com/foaf/0.1/')
+      }).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
 
     it('list.json should be parsed', function (done) {
-      var parser = new JsonLdParser();
+      var parser = new JsonLdParser()
 
-      testUtils.p.readFile('support/list.json', __dirname)
-        .then(function (list) {
-          return parser.parse(list, null, 'https://www.example.com/list')
-        })
-        .then(function (graph) {
-          return testUtils.p.assertGraphEqual(graph, testData.listGraph);
-        })
-        .then(function () {
-          done()
-        })
-        .catch(function (error) {
-          done(error);
-        });
-    });
-  });
-});
+      testUtils.p.readFile('support/list.json', __dirname).then(function (list) {
+        return parser.parse(list, null, 'https://www.example.com/list')
+      }).then(function (graph) {
+        return testUtils.p.assertGraphEqual(graph, testData.listGraph)
+      }).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
+  })
+})
 
